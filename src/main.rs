2713d765f36a9fc2c54a4ac2d4ecda9dabedc653
NEYAZ NAFIZ <neyaz@aintron.com>
2023-07-02 22:28:46 +0600

    drive model

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 mod drive_name;
+mod drive_model;
 
 use std::process::Command;
 
 fn main()  {
    drive_serial_number();
   println!("{}",  drive_name::drive_name());
+  println!("{}",  drive_model::drive_model());
 }
 
 fn drive_serial_number() {
   let output = Command::new("wmic")
   .arg("diskdrive")
   .arg("get")
-  .arg("name,")
   .arg("size,")
   .arg("model,")
   .arg("serialnumber")
